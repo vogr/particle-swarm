51f4829257c2c95bac9a3bd6c323829c741d68f2
Gavin Gray <gavinleroy6@gmail.com>
2022-05-14 15:31:52 +0200

    gitignore for object files

--- tests/src/TestUtils.jl
+++ tests/src/TestUtils.jl
@@ -2,15 +2,27 @@ module TestUtils
 
 export column_major_to_row, array_to_column_major
 
-# NOTE M must be a square matrix
-function column_major_to_row(M::Matrix{T})::Vector{T} where {T}
-    return collect(reshape(transpose(M), length(M)))
-end
+# NOTE works on any size matrix
+change_major_order(X::AbstractArray, sizes...=size(X)...) =
+    permutedims(reshape(X, sizes...), length(sizes):-1:1)
 
-# NOTE `N` is the side length of what should be an NxN matrix
-function array_to_column_major(A::Array{T}, N::Int64)::Matrix{T} where {T}
-    return collect(transpose(reshape(A, (N, N))))
-end
+column_major_to_row(M::AbstractArray)::AbstractVector =
+    collect(reshape(change_major_order(M), length(M)))
+
+# NOTE no default `sizes` is given because it would default to a vector
+array_to_column_major(A::AbstractArray, sizes...)::AbstractArray =
+    collect(change_major_order(reshape(A, sizes)))
+
+# # NOTE M must be a square matrix
+# function column_major_to_row(M::Matrix{T})::Vector{T} where {T}
+#     return collect(reshape(transpose(M), length(M)))
+# end
+
+# # NOTE `N` is the side length of what should be an NxN matrix
+# function array_to_column_major(A::Array{T}, N::Int64)::Matrix{T} where {T}
+#     return collect(transpose(reshape(A, (N, N))))
+# end
+
 
 function ptr_to_column_major(A::Ptr{T}, N::Int64)::Matrix{T} where {T}
     return collect(transpose(reshape(
