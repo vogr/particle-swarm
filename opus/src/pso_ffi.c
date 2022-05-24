0bca9f085f9d278bea22d54f4b29b7ae41ae5b52
vogr <contact@vogier.fr>
2022-05-24 12:58:24 +0200

    Fix FFI after PSO_X  changes

--- opus/src/pso_ffi.c
+++ opus/src/pso_ffi.c
@@ -3,11 +3,11 @@
 
 double * get_current_particle_positions(struct pso_data_constant_inertia * pso)
 {
-    return PSO_X(pso, pso->time, 0);
+    return pso->x;
 }
 
 
 double * get_current_particle_velocities(struct pso_data_constant_inertia * pso)
 {
-    return PSO_V(pso, 0);
+    return pso->v;
 }
