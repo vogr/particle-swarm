992be770acd9c030b764e5fe3c41dc3ec7be09c4
vogr <contact@vogier.fr>
2022-06-02 10:41:58 +0200

    Fit surrogate v5: compact cache

--- opus/src/steps/fit_surrogate.c
+++ opus/src/steps/fit_surrogate.c
@@ -19,7 +19,7 @@ int prealloc_fit_surrogate(size_t max_n_phi, size_t n_P) {
   FIT_SURROGATE_PREALLOC_VERSION(max_n_phi, n_P);
 }
 
-#define DEBUG_SURROGATE 0
+#define DEBUG_SURROGATE 1
 
 // TODO: include past_refinement_points in phi !!!
 
@@ -698,6 +698,178 @@ int fit_surrogate_4(struct pso_data_constant_inertia *pso)
     double *phi_row = phi_cache + k1 * max_N_phi;
     double *Ab_row = Ab + k1 * (n_A + 1);
     memcpy(Ab_row, phi_row, n_phi * sizeof(double));
+  }
+
+  // P and tP are blocks in A
+  // P_{i,j} := A_{i,n_phi + j} = A[i * n_A + n_phi + j]
+  // tP_{i,j} := A_{n_phi + i, j} = A[(n_phi + i) * n_A + j]
+
+  for (size_t k = 0; k < n_phi; k++)
+  {
+    double *u = x_distincts + k * dimensions;
+
+    // P(p,0) = 1;
+    Ab[k * (n_A + 1) + n_phi + 0] = 1;
+    // tP(0,p) = 1;
+    Ab[(n_phi + 0) * (n_A + 1) + k] = 1;
+
+    for (int j = 0; j < dimensions; j++)
+    {
+      // P(p,1+j) = u[j];
+      Ab[k * (n_A + 1) + n_phi + j + 1] = u[j];
+      // tP(1+j,p) = u[j];
+      Ab[(n_phi + 1 + j) * (n_A + 1) + k] = u[j];
+    }
+  }
+
+  // lower right block is zeros
+  for (size_t i = n_phi; i < n_A; i++)
+  {
+    for (size_t j = n_phi; j < n_A; j++)
+    {
+      Ab[i * (n_A + 1) + j] = 0;
+    }
+  }
+
+  /********
+   * Prepare right hand side b
+   ********/
+  for (size_t k = 0; k < n_phi; k++)
+  {
+    // set b_k
+    Ab[k * (n_A + 1) + n_A] = fxd[k];
+  }
+
+  for (size_t k = n_phi; k < n_A; k++)
+  {
+    // set b_k
+    Ab[k * (n_A + 1) + n_A] = 0;
+  }
+
+#if DEBUG_SURROGATE
+  print_rect_matrixd(Ab, n_A, n_A + 1, "Ab");
+#endif
+
+  if (gaussian_elimination_solve(n_A, Ab, pso->lambda_p) < 0)
+  {
+    return -1;
+  }
+
+#if DEBUG_SURROGATE
+  print_vectord(pso->lambda_p, n_A, "x");
+#endif
+
+  return 0;
+}
+
+
+
+
+/*
+ * Make cache more dense:
+ * phi(0,1) phi(0,2) phi(1,2) phi(0,3) phi(1,3) phi(2,3) phi(0,4) ...
+ * 
+ * phi(i,j) with i<j stored at (j * (j-1) + i)
+ */
+int prealloc_fit_surrogate_5(size_t max_n_phi, size_t n_P)
+{
+  size_t max_n_A = max_n_phi + n_P;
+  // Ab size: n x n for A and n x 1 for b
+  size_t Ab_size = max_n_A * max_n_A + max_n_A;
+
+  fit_surrogate_max_N_phi = max_n_phi;
+  fit_surrogate_phi_cache = malloc(max_n_phi * (max_n_phi - 1) / 2 * sizeof(double));
+
+  fit_surrogate_Ab = malloc(Ab_size * sizeof(double));
+  fit_surrogate_x = malloc(max_n_A * sizeof(double));
+  fit_surrogate_P = malloc(max_n_phi * n_P * sizeof(double));
+  return 0;
+}
+
+int fit_surrogate_5(struct pso_data_constant_inertia *pso)
+{
+  // TODO: include past_refinement_points in phi !!!
+
+  // TODO: note that the matrix and vector barely change between the
+  //  iterations. Maybe there could be a way to re-use them?
+
+  size_t dimensions = pso->dimensions;
+  size_t popsize = pso->population_size;
+  size_t t = pso->time;
+
+  // the size of phi is the total number of _distinct_ points where
+  // f has been evaluated
+  // currently : n = x_distinct_s
+  size_t n_phi = pso->x_distinct_s;
+  double *x_distincts = pso->x_distinct;
+  double *fxd = pso->x_distinct_eval;
+
+  // the size of P is n x d+1
+  size_t n_P = dimensions + 1;
+
+  // the size of the matrix in the linear system is n+d+1
+  size_t n_A = n_phi + n_P;
+  size_t n_Ab = n_A + 1;
+
+  double *Ab = fit_surrogate_Ab;
+  double *x = fit_surrogate_x;
+
+  size_t max_N_phi = fit_surrogate_max_N_phi;
+  double *phi_cache = fit_surrogate_phi_cache;
+  /********
+   * Prepare left hand side A
+   ********/
+
+  // phi_p,q = || u_p - u_q ||
+  // currently the {u_p} = {x_i(t=j)} i=0..pop_size, j=0..time+1
+
+  // j = 0..time are precomputed,
+  // need to compute for time+1
+
+  size_t id_new_points = pso->x_distinct_idx_of_last_batch;
+
+  if (id_new_points == n_phi)
+  {
+    // There are no new points ! The surrogate is already fit !
+#if DEBUG_SURROGATE
+    printf("Skip fit_surrogate: no new evaluation position!\n");
+#endif
+    return 0;
+  }
+
+#if DEBUG_SURROGATE
+  printf("Compute distances of %zu new points\n", n_phi - id_new_points);
+#endif
+
+  for (size_t j = id_new_points; j < n_phi; j++)
+  {
+    double *u_j = x_distincts + j * dimensions;
+    double * d3_to_u_j_cached = phi_cache + j * (j - 1);
+    for (size_t i = 0; i < j ; i++)
+    {
+      double *u_i = x_distincts + i * dimensions;
+      double d2 = dist2(dimensions, u_j, u_i);
+      double d = sqrt(d2);
+      double d3 = d2 * d;
+      d3_to_u_j_cached[i] = d3;
+    }
+  }
+
+  // start new batch
+  pso->x_distinct_idx_of_last_batch = n_phi;
+
+  // Copy the distances from phi_cache to the phi block in A
+  for (size_t j = 0; j < n_phi; j++)
+  {
+    double * d3_to_u_j_cached = phi_cache + j * (j - 1);
+
+    for (size_t i = 0 ; i < j ; i++)
+    {
+      double phi_i_j = d3_to_u_j_cached[i];
+      Ab[i * n_Ab + j] = phi_i_j;
+      Ab[j * n_Ab + i] = phi_i_j;
+    }
+    Ab[j * n_Ab + j] = 0.;
   }
 
   // P and tP are blocks in A
--- opus/src/steps/fit_surrogate.h
+++ opus/src/steps/fit_surrogate.h
@@ -3,8 +3,8 @@
 #include "../pso.h"
 
 #ifndef FIT_SURROGATE_VERSION
-#define FIT_SURROGATE_VERSION fit_surrogate_4
-#define FIT_SURROGATE_PREALLOC_VERSION prealloc_fit_surrogate_4
+#define FIT_SURROGATE_VERSION fit_surrogate_5
+#define FIT_SURROGATE_PREALLOC_VERSION prealloc_fit_surrogate_5
 #endif
 
 int fit_surrogate(struct pso_data_constant_inertia *pso);
@@ -15,9 +15,11 @@ int fit_surrogate_1(struct pso_data_constant_inertia *pso);
 int fit_surrogate_2(struct pso_data_constant_inertia *pso);
 int fit_surrogate_3(struct pso_data_constant_inertia *pso);
 int fit_surrogate_4(struct pso_data_constant_inertia *pso);
+int fit_surrogate_5(struct pso_data_constant_inertia *pso);
 
 int prealloc_fit_surrogate_0(size_t max_n_phi, size_t n_P);
 int prealloc_fit_surrogate_1(size_t max_n_phi, size_t n_P);
 int prealloc_fit_surrogate_2(size_t max_n_phi, size_t n_P);
 int prealloc_fit_surrogate_3(size_t max_n_phi, size_t n_P);
 int prealloc_fit_surrogate_4(size_t max_n_phi, size_t n_P);
+int prealloc_fit_surrogate_5(size_t max_n_phi, size_t n_P);
