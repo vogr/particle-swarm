bd9367d2b4dca7fb205952c237113f9a4750944f
vogr <contact@vogier.fr>
2022-06-04 16:59:53 +0200

    Prototype for triangular_system_solve_0

--- opus/src/triangular_system_solver.h
+++ opus/src/triangular_system_solver.h
@@ -3,6 +3,8 @@
 // int triangular_system_solve(int N, double *Ab, double *x);
 int triangular_system_solve(int N, int d, double *Ab, double *x);
 
+int triangular_system_solve_0(int N, int d, double *Ab, double *x);
+
 #ifdef TEST_PERF
 
 void register_functions_TRI_SYS_SOLVE();
