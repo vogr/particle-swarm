1b6ad4f945a89968b480075c499cfb0caa91553a
Xavier0301 <xavier.servot0@gmail.com>
2022-06-05 20:34:34 +0200

    Non-final version of run_perf_tets

--- opus/src/perf_testers/run_perf_testers.h
+++ opus/src/perf_testers/run_perf_testers.h
@@ -1,8 +1,14 @@
-#pragma once 
+#pragma once
 
 #include "stdlib.h"
 
-enum range_scheme_t_ { cubic_interpolation, linear };
+enum range_scheme_t_
+{
+  cubic_interpolation,
+  linear
+};
 typedef enum range_scheme_t_ range_scheme_t;
 
-void run_perf_tests_range_cubic_interpolation(size_t start, size_t step, size_t stop, range_scheme_t scheme);
+void run_perf_tests_range_cubic_interpolation(size_t start, size_t step,
+                                              size_t stop,
+                                              range_scheme_t scheme);
